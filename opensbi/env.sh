763e0a1fe1594f781d7726f29b25c7bb4d88a8e0
bigmagic <fujin.zhao@foxmail.com>
2021-06-25 15:54:45 +0800

    add env

---
+++ opensbi/env.sh
@@ -0,0 +1,1 @@
+export CROSS_COMPILE=/home/zhaofujin/d1/toolchain-thead-glibc/riscv64-glibc-gcc-thead_20200702/bin/riscv64-unknown-linux-gnu-  PLATFORM_RISCV_ISA=rv64gcxthead FW_JUMP_ADDR=0x40200000 FW_TEXT_START=0x40000000
